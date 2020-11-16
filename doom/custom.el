ba153b6df63bf562ebbfa990fde25a452b83f28a
Em Jones <em@emtech.cloud>
2020-11-16 10:35:54 -0600

    update doom

--- doom/custom.el
+++ doom/custom.el
@@ -5,20 +5,19 @@
  ;; If there is more than one, they won't work right.
  '(org-agenda-files
    '("/home/em/org/calendar.org" "/home/em/org/collection.org" "/home/em/org/emacs.org" "/home/em/org/example.org" "/home/em/org/gtd.org" "/home/em/org/journal.org" "/home/em/org/linked.org" "/home/em/org/mentorship.org" "/home/em/org/orgmode.org" "/home/em/org/people.org" "/home/em/org/pomodoro.org" "/home/em/org/projects.org" "/home/em/org/reading.org" "/home/em/org/vc-ref.org"))
- '(package-selected-packages
-   '(org-roam-server projectile-git-autofetch org-pomodoro org-journal-list org-roam org-journal))
- '(org-roam-server-host "127.0.0.1")
- '(org-roam-server-port 8080)
  '(org-roam-server-authenticate nil)
  '(org-roam-server-export-inline-images t)
- '(org-roam-server-serve-files nil)
- '(org-roam-server-served-file-extensions '("pdf" "mp4" "ogv"))
- '(org-roam-server-network-poll t)
+ '(org-roam-server-host "127.0.0.1")
  '(org-roam-server-network-arrows nil)
  '(org-roam-server-network-label-truncate t)
  '(org-roam-server-network-label-truncate-length 60)
  '(org-roam-server-network-label-wrap-length 20)
- )
+ '(org-roam-server-network-poll t)
+ '(org-roam-server-port 8080)
+ '(org-roam-server-serve-files nil)
+ '(org-roam-server-served-file-extensions '("pdf" "mp4" "ogv"))
+ '(package-selected-packages
+   '(ereader hnreader nov calibredb org-roam-server projectile-git-autofetch org-pomodoro org-journal-list org-roam org-journal)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -55,3 +54,12 @@
       '("r" "Book" entry (file "~/org/reading.org")
          "* %^{TITLE}\n:PROPERTIES:\n:ADDED: %<[%Y-%02m-%02d]>\n:END:%^{AUTHOR}p\n%?" :empty-lines 1))
   )
+
+(require 'calibredb)
+(setq calibredb-root-dir "~/Calibre Library")
+(setq calibredb-db-dir (expand-file-name "metadata.db" calibredb-root-dir))
+(setq calibredb-library-alist `(("~/Calibre Library")))
+(require 'nov)
+;;(require 'ereader)
+(add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode))
+;; (add-to-list 'auto-mode-alist '("\\.epub\\'" . ereader-mode))
